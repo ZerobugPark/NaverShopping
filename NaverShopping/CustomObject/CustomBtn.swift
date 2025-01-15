10b0d057fbb1b29f46b577b9dab91154fcb93872
young <zerobugpark@gmail.com>
2025-01-15 21:34:40 +0900

    커스텀 버튼 및 레이블 수정

--- NaverShopping/CustomObject/CustomBtn.swift
+++ NaverShopping/CustomObject/CustomBtn.swift
@@ -5,4 +5,45 @@
 //  Created by youngkyun park on 1/15/25.
 //
 
-import Foundation
+import UIKit
+
+class CustomBtn: UIButton {
+    
+    init(title: String, status: Bool, tagNum: Int) {
+        super.init(frame: .zero)
+        
+        setTitle(title, for: .normal)
+        if status {
+            setTitleColor(.black, for: .normal)
+            backgroundColor = .white
+        } else {
+            setTitleColor(.white, for: .normal)
+            backgroundColor = .black
+        }
+        tag = tagNum
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 1
+        titleLabel?.font = .systemFont(ofSize: 15)
+        
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+    }
+    
+    init(imgName: String){
+        super.init(frame: .zero)
+        setImage(UIImage(systemName: imgName), for: .normal)
+        setTitleColor(.black, for: .normal)
+        backgroundColor = .clear
+        tintColor = .black
+    }
+    
+    
+
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+ 
+}
--- NaverShopping/CustomObject/CustomLabel.swift
+++ NaverShopping/CustomObject/CustomLabel.swift
@@ -5,4 +5,22 @@
 //  Created by youngkyun park on 1/15/25.
 //
 
-import Foundation
+import UIKit
+
+class CustomLabel: UILabel {
+    
+    init(title: String, fontSize: CGFloat, color: UIColor, bold: Bool) {
+        super.init(frame: .zero)
+        
+        text = title
+        textColor =  color
+        font = bold ? .boldSystemFont(ofSize: fontSize)  : .systemFont(ofSize: fontSize)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+ 
+}
