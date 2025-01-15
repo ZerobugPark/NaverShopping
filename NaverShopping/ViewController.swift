931e1b1f87c6f2f2f4bdc5a86a07d6a10084da69
young <zerobugpark@gmail.com>
2025-01-15 16:42:03 +0900

    백그라운드 이미지 및 서치바 레이아웃 추가

--- NaverShopping/ViewController.swift
+++ NaverShopping/ViewController.swift
@@ -7,15 +7,22 @@
 
 import UIKit
 
+import Alamofire
+import SnapKit
+
+
 class ViewController: UIViewController {
 
     let searchBar = UISearchBar()
+    let bgImage = UIImageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupConfigure()
         view.backgroundColor = .black
         navigationItem.title = "도봉쇼핑쇼핑"
+        
+        searchBar.delegate = self
     }
 
     
@@ -25,18 +32,57 @@ class ViewController: UIViewController {
 
 extension ViewController: ConfigureView {
     
+    func setupConfigure() {
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
     func configureHierarchy() {
         view.addSubview(searchBar)
+        view.addSubview(bgImage)
     }
     
     func configureLayout() {
-        <#code#>
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(4)
+            
+        }
+        bgImage.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func configureView() {
-        <#code#>
+        let placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: placeholder,attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
+        searchBar.layer.borderWidth = 10
+        searchBar.searchTextField.backgroundColor = #colorLiteral(red: 0.1098035797, green: 0.1098041758, blue: 0.122666128, alpha: 1)
+        searchBar.searchTextField.layer.cornerRadius = 10
+        searchBar.searchTextField.clipsToBounds = true
+        searchBar.searchTextField.leftView?.tintColor = .lightGray
+        searchBar.searchTextField.tokenBackgroundColor = .white
+        searchBar.searchBarStyle = .minimal
+       
+
+        bgImage.image = UIImage(named: "flex")
     }
     
     
 }
 
+extension ViewController: UISearchBarDelegate {
+    
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        print(#function)
+    }
+    
+    // search button clicked
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(#function)
+        
+        view.endEditing(true)
+    }
+}
