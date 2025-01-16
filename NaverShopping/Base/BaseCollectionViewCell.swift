74aa219fa28f69a746cb0e4fa91b022d0ebd4361
young <zerobugpark@gmail.com>
2025-01-16 21:50:15 +0900

    서치바 및 이미지뷰 레이아웃 구성
    [Add]
    -

--- NaverShopping/Base/BaseCollectionViewCell.swift
+++ NaverShopping/Base/BaseCollectionViewCell.swift
@@ -9,4 +9,32 @@ import UIKit
 
 class BaseCollectionViewCell: UICollectionViewCell {
     
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+        
+    }
+    
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
