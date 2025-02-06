75228e17a9f18dcf0ff5d3fb6b9db9a07e8c0acf
young <zerobugpark@gmail.com>
2025-02-06 19:08:52 +0900

    [기능 구현]
    검색 기능 구현

--- NaverShopping/MVVM/ResultView/ResultViewController.swift
+++ NaverShopping/MVVM/ResultView/ResultViewController.swift
@@ -7,23 +7,114 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
+final class ResultViewController: UIViewController {
 
+    
+    let resultView = ResultView()
+    let resultModel = ResultViewModel()
+    
+    override func loadView() {
+        view = resultView
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupDelegate()
+        addTargetButton()
+        bindData()
+        navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupDelegate() {
+        resultView.collectionView.delegate = self
+        resultView.collectionView.dataSource = self
+        //resultView.collectionView.prefetchDataSource = self
+        
+        resultView.collectionView.register(ResultViewCollectionViewCell.self, forCellWithReuseIdentifier: "ResultViewCollectionViewCell")
     }
-    */
+    
+    private func addTargetButton() {
+        for i in 0..<resultView.buttons.count {
+            resultView.buttons[i].addTarget(self, action: #selector(filterButtonTapped), for: .touchUpInside)
+        }
+    }
+ 
+    private func bindData() {
+        
+        resultModel.outputSearchText.bind { [weak self] text in
+            print("outputSearchText bind")
+            
+            self?.navigationItem.title = text
+            self?.resultModel.inputViewDidLoad.value = text
+            
+        }
+        
+        resultModel.outputItems.lazyBind { [weak self] _ in
+            self?.resultView.collectionView.reloadData()
+        }
+        
+        resultModel.outputTotal.lazyBind { [weak self] text in
+            self?.resultView.resultCountLabel.text = text
+        }
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print(#function)
+    }
+    
+    deinit {
+        print("ResultViewController deinit")
+    }
+    
+}
 
+
+// MARK: - CollectionView Delegate
+extension ResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return resultModel.outputItems.value.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ResultViewCollectionViewCell.id, for: indexPath) as? ResultViewCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        let item = resultModel.outputItems.value[indexPath.item]
+        cell.updateItemList(item: item)
+        
+        return cell
+    }
+    
+    
+}
+
+// MARK: - Objc Function
+
+extension ResultViewController {
+    
+    @objc private func filterButtonTapped(_ sender: UIButton) {
+        changeButtonColor(tag: sender.tag)
+        resultModel.inputFilterButtonTapped.value = sender.tag
+    }
+    
+    
+    private func changeButtonColor(tag: Int) {
+
+        for i in 0..<resultView.buttons.count {
+            if i == tag {
+                resultView.buttons[i].configuration?.baseForegroundColor = .black
+                resultView.buttons[i].configuration?.baseBackgroundColor = .white
+
+            } else {
+                resultView.buttons[i].configuration?.baseForegroundColor = .white
+                resultView.buttons[i].configuration?.baseBackgroundColor = .black
+
+            }
+        }
+    }
 }
--- NaverShopping/MVVM/ResultView/ResultViewModel.swift
+++ NaverShopping/MVVM/ResultView/ResultViewModel.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+
+class ResultViewModel {
+    
+    
+    let outputSearchText: Observable<String?> = Observable(nil)
+    
+    let outputItems: Observable<[Item]> = Observable([])
+    let outputTotal: Observable<String> = Observable("")
+    
+
+    let inputViewDidLoad: Observable<String?> = Observable((""))
+    let inputFilterButtonTapped: Observable<Int> = Observable(0)
+    
+    private var query = ""
+    
+    init() {
+        print("ResultViewModel init")
+        
+        inputViewDidLoad.lazyBind { [weak self] text in
+            guard let text = text else {
+                print("바인딩 오류")
+                return
+            }
+            self?.callRequest(text: text, sort: Sorts.sim.rawValue)
+        }
+        
+        inputFilterButtonTapped.lazyBind { [weak self] value in
+            let sort = self?.findFilterTag(val: value)
+            self?.callRequest(text: self!.query, sort: sort!)
+        }
+        
+    }
+    
+    func callRequest(text: String, sort: String) {
+        
+
+        query = text
+        
+        NetworkManagerMVVM.shared.callRequest(api: .getInfo(query: query, display: 100, sort: sort, startIndex: 1), type: NaverShoppingInfo.self, completionHandler: { response in
+            
+            switch response {
+            case .success(let value):
+                self.outputItems.value = value.items
+                self.outputTotal.value = value.total.formatted() + " 개의 검색 결과"
+                
+                //print(value)
+            case.failure(let error):
+                print(error)
+            }
+            
+        })
+    }
+    
+    private func findFilterTag(val: Int) -> String {
+        print(val)
+        switch val {
+        case 0:
+           return Sorts.sim.rawValue
+        case 1:
+            return Sorts.date.rawValue
+        case 2:
+            return Sorts.dsc.rawValue
+        case 3:
+            return Sorts.asc.rawValue
+        default:
+           return Sorts.sim.rawValue
+        }
+    }
+    
+    
+    deinit {
+        print("ResultViewModel deinit")
+    }
+    
+    
+    
+    
+    
+}
